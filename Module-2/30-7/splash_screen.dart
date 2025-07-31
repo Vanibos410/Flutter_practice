2bdc096fe3a7c9e2adb6d37ef2867585c4374468
VaniBosamiya <128339420+Vanibos410@users.noreply.github.com>
2025-07-31 12:56:19 +0530

    c

---
+++ Module-2/30-7/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:splash_screen/login.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    // super.initState();
+    Timer(Duration(seconds: 3), () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen())));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // child: Image.asset("assets/a.jpg"),
+        child: Lottie.asset("assets/b.json"),
+      ),
+    );
+  }
+}
